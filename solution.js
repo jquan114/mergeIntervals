3b10d4d8543194d45ed431762120d952a77d1324
JQUAN COLLINS <104362301+jquan114@users.noreply.github.com>
2023-01-26 10:17:52 -0500

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,5 +1,5 @@
-Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals,
-and return an array of the non-overlapping intervals that cover all the intervals in the input.
+// Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals,
+// and return an array of the non-overlapping intervals that cover all the intervals in the input.
 
 var merge = function(intervals) {
       intervals.sort((a, b) => a[0] - b[0]);
